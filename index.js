ca6d1d2d12b14127c91779fe7c82005db0c1c31a
Doodle173 <dkgray01@gmail.com>
2024-01-14 15:03:16 -0500

    parse revisions table

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
 
 try {
-  fs.unlinkSync('./test.db');
-  console.log('successfully deleted ./test.db');
+    fs.unlinkSync('./test.db');
+    console.log('successfully deleted ./test.db \n');
 } catch (err) {
-  // handle the error
-  console.log(err);
+    // handle the error
+    console.log(err);
 }
 
 
@@ -17,15 +17,52 @@ const db = new sqlite3.Database('./test.db');
 db.serialize(() => {
     var obj = JSON.parse(fs.readFileSync('data/jmdict-eng-3.5.0.json', 'utf8'));
 
-    console.log('successfully created ./test.db');
-    db.run("CREATE TABLE metadata (version TEXT)");
+    console.log('successfully created ./test.db \n');
+    db.run("CREATE TABLE metadata (version TEXT, build_date TEXT, commonOnly INTEGER)");
 
-    const stmt = db.prepare(`INSERT INTO metadata VALUES ("${obj.version}")`);
-    
+    var common = 0;
+
+    if(obj.commonOnly == true){
+        common = 1;
+    }
+
+    /**
+     * Create the metadata table and insert the correct data..
+     */
+    console.log("Creating metadata table...");
+    var stmt = db.prepare(`INSERT INTO metadata(version, build_date, commonOnly) VALUES ("${obj.version}", "${obj.dictDate}", ${common})`);
     stmt.run();
     stmt.finalize();
+    console.log("Metadata table has been created. \n");
 
+
+    console.log("Creating languages table...");
+
+    /**
+     * Create the languages table and insert the correct data..
+     */
+    db.run("CREATE TABLE languages (language TEXT)");
+    obj.languages.forEach(async (lang) => {
+        stmt = db.prepare(`INSERT INTO languages(language) VALUES ("${lang}")`);
+        stmt.run();
+        stmt.finalize();
+    });
+
+    console.log("Languages table has been created. \n");
+
+    /**
+     * Create the revisions table and insert the correct data..
+     */
+    console.log("Creating revisions table...");
+
+    db.run("CREATE TABLE revisions (revision TEXT)");
+    obj.dictRevisions.forEach(async (rev) => {
+        stmt = db.prepare(`INSERT INTO revisions(revision) VALUES ("${rev}")`);
+        stmt.run();
+        stmt.finalize();
+    });
+
+    console.log("Languages table has been created. \n");
     
-});
 
-db.close();
+});
